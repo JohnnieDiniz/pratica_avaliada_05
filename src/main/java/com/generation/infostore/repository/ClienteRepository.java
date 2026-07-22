c164617885e66dc7a487beb0372b08d09d375baf
João Vitor Diniz Alves <diniz.vitor.alves@gmail.com>
2026-07-22 17:58:00 -0300

    :sparkles: Repository e Controller criadas

---
+++ src/main/java/com/generation/infostore/repository/ClienteRepository.java
@@ -0,0 +1,13 @@
+package com.generation.infostore.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.infostore.model.Cliente;
+
+public interface ClienteRepository extends JpaRepository <Cliente, Long>{
+	
+	public List<Cliente> findAllByNomeContainingIgnoreCase(String nome);
+
+}
